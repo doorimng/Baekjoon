c9983259dca98a53a16c60dc86ad1681282933ff
doorimng <doorimng@gmail.com>
2025-07-29 01:10:40 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std ;
+typedef long long ll ;
+typedef pair <int, int> pii ;
+#define INF INT_MAX
+
+ll Pow(int n) {
+    ll ans = 1 ;
+    while ( n > 0 ) {
+        ans = (ans * 31) % 1234567891 ;
+        n -- ;
+    }
+    return ans ;
+}
+
+int main() {
+
+    cin.tie(0)->sync_with_stdio(false) ;
+
+    int n, ans = 0 ;
+    string s ;
+    cin >> n >> s ;
+
+    for ( int i = 0 ; i < n ; i ++ ) {
+        ans += ((s[i] - 'a' + 1) * Pow(i)) % 12334567891 ;
+    }
+
+    cout << ans % 1234567891 ;
+    
+    return 0 ;
+}
