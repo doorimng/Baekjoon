493f43eced22f315b02c0bde0e4c56a7f90e7eff
doorimng <doorimng@gmail.com>
2025-08-23 21:56:46 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std ;
+typedef long long ll ;
+typedef pair <int, int> pii ;
+#define INF INT_MAX
+
+int main() {
+
+    cin.tie(0)->sync_with_stdio(false) ;
+
+    int n ;
+    cin >> n ;
+
+    cout << pow(2, n) ;
+
+    return 0 ;
+}
