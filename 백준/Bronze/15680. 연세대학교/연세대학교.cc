d49c55d2ae764dbe258ba246dee6da6f72fd2c20
doorimng <doorimng@gmail.com>
2025-08-31 01:20:07 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std ;
+typedef long long ll ;
+typedef pair <int, int> pii ;
+#define INF INT_MAX
+
+int main() {
+
+    cin.tie(0)->sync_with_stdio(false) ;
+
+    int n ;
+    cin >> n ;
+
+    if ( n ) cout << "Leading the Way to the Future" ;
+    else cout << "YONSEI" ;
+
+    return 0 ;
+}
