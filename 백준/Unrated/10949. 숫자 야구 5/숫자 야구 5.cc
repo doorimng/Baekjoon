2ece8ef7d72559ecf11a248f70ed7417819b2099
doorimng <doorimng@gmail.com>
2025-09-03 15:00:23 +0900

    [Unrated] Title: 숫자 야구 5, Time: 0 ms, Memory: 2020 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Unrated/10949. 숫자 야구 5/숫자 야구 5.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std ;
+typedef long long ll ;
+typedef pair <int, int> pii ;
+#define INF INT_MAX
+
+int main() {
+
+    cin.tie(0)->sync_with_stdio(false) ;
+
+    cout << "1234" ;
+
+    return 0 ;
+}
