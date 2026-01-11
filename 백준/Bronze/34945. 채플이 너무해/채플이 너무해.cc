bbe0b57868bd83b688571c32f474d2e53ad9e0c6
doorimng <doorimng@gmail.com>
2026-01-11 22:03:58 +0900

    [Bronze V] Title: 채플이 너무해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34945. 채플이 너무해/채플이 너무해.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std ;
+typedef long long ll ;
+typedef pair <int, int> pii ;
+#define INF INT_MAX
+
+int main() {
+    
+    cin.tie(0)->sync_with_stdio(false) ;
+    
+    int n ;
+    cin >> n ;
+
+    if ( n >= 6 ) cout << "Success!" ;
+    else cout << "Oh My God!" ;
+
+    return 0 ;
+}
