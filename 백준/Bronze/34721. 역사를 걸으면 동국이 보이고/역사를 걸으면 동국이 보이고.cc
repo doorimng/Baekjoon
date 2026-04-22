268e07b3f0ada4a7b510ce52d674f95807301532
doorimng <doorimng@gmail.com>
2026-04-23 01:09:06 +0900

    [Bronze V] Title: 역사를 걸으면 동국이 보이고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34721. 역사를 걸으면 동국이 보이고/역사를 걸으면 동국이 보이고.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std ;
+typedef unsigned long long ll ;
+typedef pair <int, int> pii ;
+#define INF INT_MAX
+
+int main() {
+    
+    cin.tie(0)->sync_with_stdio(false) ;
+
+    int n ;
+    cin >> n ;
+
+    while ( n -- ) {
+        cout << "I love DGU\n" ;
+    }
+
+    return 0 ;
+}
